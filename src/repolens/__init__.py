e6ffa2de3d891bdae4698f0c1341f4a32cda5b9d
Kjell Hedström <kjell.hedstrom@gmail.com>
2026-06-05 16:10:59 -0600

    versioning (#61)

--- src/repolens/__init__.py
+++ src/repolens/__init__.py
@@ -1,5 +1,21 @@
 """RepoLens CLI package."""
 
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
+
 __all__ = ["__version__"]
 
-__version__ = "0.1.0"
+
+def _package_version() -> str:
+    try:
+        return version("repolens")
+    except PackageNotFoundError:
+        try:
+            from repolens._version import __version__ as generated_version
+        except ModuleNotFoundError:
+            return "0+unknown"
+        return generated_version
+
+
+__version__ = _package_version()
